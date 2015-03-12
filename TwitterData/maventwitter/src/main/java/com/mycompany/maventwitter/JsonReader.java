fbd6ae0c8ef26702deaf8e0b2aef565ce7859a9e
HansHoogerwerf <hans3132@live.nl>
2015-03-12 15:31:28 +0100

    twitterfix

---
+++ TwitterData/maventwitter/src/main/java/com/mycompany/maventwitter/JsonReader.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.maventwitter;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+/**
+ *
+ * @author Hans
+ */
+public class JsonReader {
+    
+    private static String readAll(Reader rd) throws IOException {
+    StringBuilder sb = new StringBuilder();
+    int cp;
+    while ((cp = rd.read()) != -1) {
+      sb.append((char) cp);
+    }
+    return sb.toString();
+  }
+    
+    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+    InputStream is = new URL(url).openStream();
+    try {
+      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+      String jsonText = readAll(rd);
+      JSONObject json = new JSONObject(jsonText);
+      return json;
+    } finally {
+      is.close();
+    }
+  }
+
+    public static void readJsons() throws JSONException, IOException {
+        JSONObject json = readJsonFromUrl("http://ip.jsontest.com/?callback=showMyIP");
+        System.out.println(json.toString());
+//        System.out.println(json.get("id"));
+//        System.out.println(json.get("country"));
+//        System.out.println(json.get("name"));
+    }
+}
--- TwitterData/maventwitter/src/main/java/com/mycompany/maventwitter/twitterjsonparse.java
+++ TwitterData/maventwitter/src/main/java/com/mycompany/maventwitter/twitterjsonparse.java
@@ -10,13 +10,13 @@ package com.mycompany.maventwitter;
  * @author Hans
  */
 import com.google.common.collect.Lists;
-//import com.mycompany.maventwitter.WeatherData.WeatherCondition;
 import com.twitter.hbc.ClientBuilder;
 //import com.twitter.hbc.SitestreamController;
 import com.twitter.hbc.core.Client;
 import com.twitter.hbc.core.Constants;
 import com.twitter.hbc.core.Hosts;
 import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.Location;
 import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
 import com.twitter.hbc.core.event.Event;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
@@ -32,15 +32,8 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.json.simple.JSONObject;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONValue;
-
-
-
-
-
-
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class twitterjsonparse {
 
@@ -54,132 +47,75 @@ public class twitterjsonparse {
      * @param args
      * @throws java.lang.InterruptedException
      */
-    public static void main(String[] args) throws InterruptedException, IOException{
+    public static void main(String[] args) throws InterruptedException, JSONException, IOException {
         //twitter4jmodule();
         extraTestModule();
-
-//        OwmClient owm = new OwmClient();
-//        WeatherStatusResponse currentWeather = owm.currentWeatherAtCity("Rotterdam", "NL");
-//        if (currentWeather.hasWeatherStatus()) {
-//            WeatherData weather = currentWeather.getWeatherStatus().get(0);
-//            if (weather.getPrecipitation() == Integer.MIN_VALUE) {
-//                WeatherCondition weatherCondition = weather.getWeatherConditions().get(0);
-//                String description = weatherCondition.getDescription();
-//                if (description.contains("rain") || description.contains("shower")) {
-//                    System.out.println("No rain measures in Tokyo but reports of " + description);
-//                } else {
-//                    System.out.println("No rain measures in Tokyo: " + description);
-//                }
-//            } else {
-//                System.out.println("It's raining in Tokyo: " + weather.getPrecipitation() + " mm/h");
-//            }
-//        }
+        //JsonReader.readJsons();
     }
 
-    public static void twitter4jmodule() {
-        /**
-         * Set up your blocking queues: Be sure to size these properly based on
-         * expected TPS of your stream
-         */
-        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<>(100000);
-        BlockingQueue<Event> eventQueue = new LinkedBlockingQueue<>(1000);
+    public static void waetherdata() {
 
-        /**
-         * Declare the host you want to connect to, the endpoint, and
-         * authentication (basic auth or oauth)
-         */
-        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
-        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
-// Optional: set up some followings and track terms
-        List<Long> followings = Lists.newArrayList(1234L, 566788L);
-        List<String> terms = Lists.newArrayList("twitter", "api");
-        hosebirdEndpoint.followings(followings);
-        hosebirdEndpoint.trackTerms(terms);
-
-// These secrets should be read from a config file
-        ClientBuilder builder = new ClientBuilder()
-                .name("Hosebird-Client-01") // optional: mainly for the logs
-                .hosts(hosebirdHosts)
-                .authentication(hosebirdAuth)
-                .endpoint(hosebirdEndpoint)
-                .processor(new StringDelimitedProcessor(msgQueue))
-                .eventMessageQueue(eventQueue);                          // optional: use this if you want to process client events
-
-        Client hosebirdClient = builder.build();
-// Attempts to establish a connection.
-        hosebirdClient.connect();
-        int i = 0;
-        while (i < 10) {
-            String msg = null;
-            try {
-                i++;
-                msg = msgQueue.take();
-            } catch (InterruptedException ex) {
-                Logger.getLogger(twitterjsonparse.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-            System.out.println(msg);
-
-        }
-        hosebirdClient.stop();
-    }
-
-    public static void newParser() {
-
-    }
-
-    public static void newObject(){
-        ArrayList<JSONObject> clients = new ArrayList<>();
-
-        for (int i = 0; i < 10; i++) {
-            clients.add(obj);
-
-            obj = new JSONObject();
-            obj.put("name", "foo");
-            obj.put("ID", i);
-            obj.put("balance", 1000.21);
-            obj.put("is_vip", true);
-            obj.put("nickname", "client " + i);
-
-        }
-        System.out.print(clients);
     }
 
     /**
      *
      * @throws InterruptedException
+     * @throws org.json.JSONException
      */
-    public static void extraTestModule() throws InterruptedException{
+    public static void extraTestModule() throws InterruptedException, JSONException {
         BlockingQueue<String> queue = new LinkedBlockingQueue<>(10000);
         StatusesFilterEndpoint endpoint = new StatusesFilterEndpoint();
         // add some track terms
-        endpoint.trackTerms(Lists.newArrayList("blijdorp", "blij dorp", "@rotterdamzoo", "rotterdamzoo","twitter"));
+        endpoint.trackTerms(Lists.newArrayList("blijdorp","@rotterdamzoo","#blijdorp"));
 
-    // Authentication auth = new BasicAuth(username, password);
+        // Authentication auth = new BasicAuth(username, password);
         // Create a new BasicClient. By default gzip is enabled.
         Client cleint01 = new ClientBuilder()
                 .hosts(Constants.STREAM_HOST)
                 .endpoint(endpoint)
                 .authentication(hosebirdAuth)
                 .processor(new StringDelimitedProcessor(queue))
-                
                 .build();
 
         // Establish a connection
         cleint01.connect();
-       
+        JSONObject json;
+        JSONObject userJson;
+        double text = 0;
+        double noText = 0;
         // Do whatever needs to be done with messages
-        for (int msgRead = 0; msgRead < 1000; msgRead++) {
+        for (int msgRead = 0; msgRead < 10; msgRead++) {
             String msg = queue.take();
-            System.out.println(msg);
-            Object obj01=JSONValue.parse(msg);
-            JSONArray array=(JSONArray)obj01;
-            System.out.println(array.get(1));
-             
+            //System.out.println(msg);
+
+            json = new JSONObject(msg);
+            
+            //System.out.println(json.toString());
+            
+            try{
+            String twitterText = json.getString("text");
+            
+            String twitterUser = json.getString("user");
+            
+            
+            userJson = new JSONObject(twitterUser);
+            
+            String twitterName = userJson.getString("name");
+            
+            System.out.println(twitterName+": " + twitterText);
+            text++;
+            }
+            catch(Exception e)
+            {
+                
+                noText++;
+            }
             
         }
 
         cleint01.stop();
+        double totalTweets = text + noText;
+        double percentage = text / totalTweets * 100;
+        System.out.println("Of the " + totalTweets + " tweets, " + text + " had text,that is " + percentage + "% of the tweets.");
     }
-
 }
