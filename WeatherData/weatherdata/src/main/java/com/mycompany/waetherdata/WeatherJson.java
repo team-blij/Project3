e6b21cc6f7b1d55364efc9fe08547c11e2e8b611
HansHoogerwerf <hans3132@live.nl>
2015-03-16 09:52:03 +0100

    weather enzo

--- WeatherData/weatherdata/src/main/java/com/mycompany/waetherdata/WeatherJson.java
+++ WeatherData/weatherdata/src/main/java/com/mycompany/waetherdata/WeatherJson.java
@@ -16,6 +16,8 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -44,20 +46,38 @@ public class WeatherJson {
   }
 
 
-  public static void main(String[] args) throws IOException, JSONException {
-    //JSONObject json = readJsonFromUrl("http://api.openweathermap.org/data/2.5/weather?q=Rotterdam,nl&APPID=a05c0cf00ca0c2fa9fb79f6aadbde01d");
-      String landcode = "nl";
-      String stad = "Rotterdam";
-      String jsonurl = "http://api.openweathermap.org/data/2.5/weather?q=;"+stad+","+landcode+"&APPID=a05c0cf00ca0c2fa9fb79f6aadbde01d";
-    
-    
-    System.out.println(getJsondata(jsonurl).toString());
-    System.out.println(getJsondata(jsonurl).get("name"));
+  public static void main(String[] args)   {
+      WeatherData();
   }
     public static JSONObject getJsondata(String url) throws IOException, JSONException
     {
        JSONObject json = readJsonFromUrl(url);
        
        return json;
+    }
+    
+    public static void WeatherData()
+    {
+       try {
+          //JSONObject json = readJsonFromUrl("http://api.openweathermap.org/data/2.5/weather?q=Rotterdam,nl&APPID=a05c0cf00ca0c2fa9fb79f6aadbde01d");
+          String landcode = "nl";
+          String stad = "Rotterdam";
+          String jsonurl = "http://api.openweathermap.org/data/2.5/weather?q=;"+stad+","+landcode+"&APPID=a05c0cf00ca0c2fa9fb79f6aadbde01d";
+          
+          
+          try {
+              System.out.println(getJsondata(jsonurl).toString());
+          } catch (IOException ex) {
+              Logger.getLogger(WeatherJson.class.getName()).log(Level.SEVERE, null, ex);
+          } catch (JSONException ex) {
+              Logger.getLogger(WeatherJson.class.getName()).log(Level.SEVERE, null, ex);
+          }
+          System.out.println(getJsondata(jsonurl).get("name"));
+      } catch (IOException ex) {
+          Logger.getLogger(WeatherJson.class.getName()).log(Level.SEVERE, null, ex);
+      } catch (JSONException ex) {
+          System.out.println("Requesting data again!");
+          WeatherData();
+      } 
     }
 }
