fe12ad22c0666baf75c4ca8009efec66977e657c
Elize <0891435@hr.nl>
2015-03-16 12:22:02 +0100

    New Keys

--- Twitter_Elize/src/GetTwitter.java
+++ Twitter_Elize/src/GetTwitter.java
@@ -1,6 +1,7 @@
 import twitter4j.*;
 import twitter4j.conf.*;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 
@@ -10,32 +11,27 @@ import java.util.ArrayList;
 public class GetTwitter {
     ConfigurationBuilder cb = new ConfigurationBuilder();
     public Twitter twitterInstance;
-    //public Query query = new Query("rotterdamzoo " + "Blijdorp");
-    public Query query = null;
+    public Query query = new Query("rotterdamzoo :)" + "Blijdorp");
+
     public User Blijdorp = null;
-    private ArrayList<String> blijdorpAssets= new ArrayList<String>();
+    String[] blijdorpAssets = {"rotterdamzoo oceanium " , "@rotterdamzoo rivierahal :)"};
+
 
 
     public void setup(){
-
-
-            cb.setOAuthConsumerKey("hYiZEb7KIq8xRzouCcHMHV8KG");
-            cb.setOAuthConsumerSecret("KwBo9KEfb28iqx4MCzJLyfKiIUoKSY68Nw9un5xGKWSjb6MK8m");
-            cb.setOAuthAccessToken("543574610-XMJxyBrK7rIpWoSAZwy2je3EXdsIUfiwtW0vvGX2");
-            cb.setOAuthAccessTokenSecret("PTfk8qYKw711DQv9jsW6OTjQSZrUr9mw3M6JfP2QNCjIF");
-            //All keys are from maventwitter
+            cb.setOAuthConsumerKey("BiTybWWcHtZfNDF6k27XQysZo");
+            cb.setOAuthConsumerSecret("SarY78keJf2LRJJS38xFbpjizvkFbKDxg0eULV5NJQ36TxJW07");
+            cb.setOAuthAccessToken("3092334623-zofIrzyqSpVlnYHq42ozb2ubz9uU3d6D5CmcMfA");
+            cb.setOAuthAccessTokenSecret("dTlH0Qa1kPbwXHCa5xkkRhkIyLeD2JvP2bqd1UhNeioID");
+            //All keys are private! >:(
             twitterInstance = new TwitterFactory(cb.build()).getInstance();
-
-
             getBestAsset();
             //fetchAndDrawTweets();
-
 
     }// end of setup()
 
     private void fetchAndDrawTweets(){
         try{
-
             QueryResult result = twitterInstance.search(query);
             ArrayList tweets = (ArrayList) result.getTweets();
             for (int i = 0; i < result.getCount(); i++){
@@ -47,9 +43,7 @@ public class GetTwitter {
                 //The tweet
                 String geoLocation = status.getUser().getLocation();
                 //the location of the user
-
                 System.out.println(status + " " + user + " " + message);
-
             }
         }catch(TwitterException te){
             System.out.println("Couldn't connect!");
@@ -71,13 +65,17 @@ public class GetTwitter {
 //  --------------------------------------------------------------------------------------------------------------------
 //  Welk onderdeel van diergaarde Blijdorp vinden de bezoekers het leukst?
     private void getBestAsset() {
-        blijdorpAssets.add("@rotterdamzoo :) oceanium");
-        blijdorpAssets.add("@rotterdamzoo :) riverahal");
-        for (String p : blijdorpAssets) {
+
+        try {
+            for (String p : blijdorpAssets) {
                 query = new Query(p);
                 fetchAndDrawTweets();
             }
 
+        }catch(IndexOutOfBoundsException ex){
+            System.out.println("Geen resultaten gevonden...");
+
+        }
     }// end of getBestAsset
 
 }// end of GetTwitter class
